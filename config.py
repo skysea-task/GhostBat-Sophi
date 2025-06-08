a4820b5d0051eec8cadcb03c8ac6c74f91f64a08
rajs2695 <rajeshrakis2695@gmail.com>
2025-06-08 16:46:39 +0530

    config.py

--- config.py
+++ config.py
@@ -4,5 +4,5 @@ OWNER_ID = RAUSHAN.me.id
 
 # SETTINGS 
 Do_you_need_warnings = False # You will Get some Warning messages Using by some Commands. if you don't like change it False.
-SUDO_USERS_ID = [8071602126] # Sudo user can Use some Commands But not all Maybe Try to add Your GF.
+SUDO_USERS_ID = [1281282633] # Sudo user can Use some Commands But not all Maybe Try to add Your GF.
 BOTS_ALLOWED_TO_WORK_IN_BUSY_COMMANDS = False # I will add soon this.
